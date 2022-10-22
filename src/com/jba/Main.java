0fe42b97a19cc40e0c83c7b77d93c381cf9deeed
Pratyay Ganguli <pratyay.ganguli@ziroh.com>
2022-10-22 21:51:03 +0530

    Added project files

---
+++ src/com/jba/Main.java
@@ -0,0 +1,17 @@
+package com.jba;
+
+import com.jba.collections.ArrayListOperations;
+
+/**
+ * @author Pratyay Ganguli
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayListOperations operations = new ArrayListOperations();
+        var list = operations.create();
+
+
+    }
+}
---
+++ src/com/jba/collections/ArrayListOperations.java
@@ -0,0 +1,15 @@
+package com.jba.collections;
+
+import java.util.ArrayList;
+
+/**
+ * @author Pratyay Ganguli
+ */
+
+public class ArrayListOperations {
+
+    public ArrayList create(){
+        return new ArrayList();
+    }
+
+}
